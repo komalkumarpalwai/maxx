d23e35177ee9a24129619195ff599414fd71f2b9
komal kumar <152193731+komalkumarpalwai@users.noreply.github.com>
2025-08-27 14:54:56 +0530

    day 5 client side web  fixed

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const logoutRoutes = require('./routes/logout');
 dotenv.config();
 
 const app = express();
-const PORT = 5001;
+const PORT =process.env.PORT || 5001;
 
 // Middleware
 app.use(cors({
